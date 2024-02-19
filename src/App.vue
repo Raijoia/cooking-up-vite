2d0e1678aa1c8c6cd35f3395aeffc86746fc533a
Raí Joia <megac154@gmail.com>
2024-02-19 18:48:37 -0300

    feat: conteudo principal criado com a diretiva v-for

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script lang="ts">
   import Banner from './components/Banner.vue';
+  import ConteudoPrincipalVue from './components/ConteudoPrincipal.vue';
 
   export default {
     components: {
-      Banner,
-    },
+    Banner,
+    ConteudoPrincipalVue
+},
   };
 </script>
 
 <template>
   <Banner />
+  <ConteudoPrincipalVue />
 </template>
